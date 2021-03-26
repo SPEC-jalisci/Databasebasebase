e50729d0879e77836b23f49937b42914cc28d178
JanKrzemien <68891867+JanKrzemien@users.noreply.github.com>
2021-03-26 13:22:02 +0100

    Update 4.sql

--- zapytania/4.sql
+++ zapytania/4.sql
@@ -5,5 +5,6 @@ INNER JOIN offer ON offer_customer.offer_id = offer.id
 INNER JOIN product ON offer.product_id = product.id
 INNER JOIN product_category ON product.category_id = product_category.id
 GROUP BY customer.id 
-HAVING (STRING_AGG(product_category.value,',') LIKE '%hardware%' AND STRING_AGG(product_category.value,',') LIKE '%licencja_3%')
-OR (STRING_AGG(product_category.value,',') LIKE '%hardware%' AND STRING_AGG(product_category.value,',') LIKE '%licencja_rok%');
+HAVING STRING_AGG(product_category.value,',') LIKE '%hardware%'
+		AND (STRING_AGG(product_category.value,',') LIKE '%licencja_3%'
+		OR STRING_AGG(product_category.value,',') LIKE '%licencja_rok%')
