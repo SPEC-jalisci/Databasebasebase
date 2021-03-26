64116e9788831cf10a76088e19ffba4fc2af7c12
JanKrzemien <68891867+JanKrzemien@users.noreply.github.com>
2021-03-26 13:56:02 +0100

    changing equal to between

--- zapytania/11.sql
+++ zapytania/11.sql
@@ -3,7 +3,7 @@ WITH dane AS (
     FROM offer_customer
     INNER JOIN sale ON offer_customer.id = sale.offer_customer_id
     INNER JOIN customer ON offer_customer.customer_id = customer.id
-    WHERE (sale.transaction_date >= '2019-01-01' AND sale.transaction_date <= '2019-12-31') AND customer.invoice_address_id != customer.shipment_address_id
+    WHERE (sale.transaction_date BETWEEN '2019-01-01' AND '2019-12-31') AND customer.invoice_address_id != customer.shipment_address_id
     GROUP BY customer.id
     HAVING COUNT(sale.transaction_date) >= 3
 )
--- zapytania/14.sql
+++ zapytania/14.sql
@@ -3,6 +3,6 @@ FROM offer_customer
 INNER JOIN sale ON offer_customer.offer_id = sale.id
 INNER JOIN offer ON offer_customer.customer_id = offer.id
 INNER JOIN product ON product.id = offer.product_id
-WHERE product.id >= 5 AND product.id <= 50 /* przedział otwarty czy zamknięty??? */
+WHERE product.id BETWEEN 5 AND 50
 GROUP BY product.id
 HAVING MAX(offer.quantity) = 1
